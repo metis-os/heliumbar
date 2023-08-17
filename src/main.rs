63f13ca3cf9b98df509fea8a639fa8deea8b8bd2
Santosh kurmi <xantosh121@gmail.com>
2023-08-17 22:22:04 +0545

    added css and button widget support

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ mod ui;
 mod utils;
 mod widgets;
 
-use gtk::{
-    prelude::{ApplicationExt, ApplicationExtManual},
-    traits::CssProviderExt,
-};
+use gtk::prelude::{ApplicationExt, ApplicationExtManual};
+
+use crate::ui::elements::build_ui;
 
 fn main() -> gtk::glib::ExitCode {
     const APP_ID: &str = "com.heliumbar";
 
     let app = gtk::Application::builder().application_id(APP_ID).build();
 
-    app.connect_activate(|app| {
-        ui::elements::build_ui(app);
-    });
+    app.connect_activate(build_ui);
     app.run()
 }
