9fb8dec97c271193adc66e69d805cb8297163c6d
pwnwriter <hey@pwnwriter.xyz>
2023-08-19 19:52:47 +0545

    feat(modules): dbus to show utils

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
+use gtk::prelude::{ApplicationExt, ApplicationExtManual};
 mod config;
+mod modules;
 mod ui;
 mod utils;
 mod widgets;
 
-use gtk::prelude::{ApplicationExt, ApplicationExtManual};
-use crate::ui::elements::build_ui;
-
-fn main() -> gtk::glib::ExitCode {
+#[tokio::main]
+async fn main() -> gtk::glib::ExitCode {
     const APP_ID: &str = "com.heliumbar";
-
     let app = gtk::Application::builder().application_id(APP_ID).build();
-
-    // utils::regex_matcher::format();
-    app.connect_activate(build_ui);
+    app.connect_activate(crate::ui::elements::build_ui);
     app.run()
 }
